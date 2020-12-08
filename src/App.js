e71ff6a293d28bb122b3a6f0835abeef0d466d45
DUONGNAMTRUONG <namtruongdev@gmail.com>
2020-12-08 17:36:27 +0700

    updaste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import config from './360_assets/COR49_Billancourt/COR49_Billancourt.xml';
 
-function App() {
+import './imagerotator/html/js/imagerotator.js';
+
+const App = () => {
+  console.log(config);
+  useEffect(() => {
+    window.$('#wr360PlayerId').rotator({
+      licenseFileURL: 'license.lic',
+      configFileURL: `${config}`,
+      graphicsPath: 'imagerotator/html/img/retina',
+      alt: '360 degree view ALT description',
+      googleEventTracking: false,
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section
+        id="wr360PlayerId"
+        className="wr360_player"
+        style={{ backgroundColor: '#FFFFFF' }}
+      ></section>
     </div>
   );
-}
+};
 
 export default App;
