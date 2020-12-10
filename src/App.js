8ee8df31a08d47ac890a3375c994ba2e07a922a9
NAM TRUONG DUONG <namtruong@namtruong-pc>
2020-12-11 02:53:32 +0700

    fix css error & add hotspot

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import { useEffect } from 'react';
-import './App.css';
 
 import './assets/js/imagerotator.js';
-import config from './assets/projects/projects-1/overall/published/360_assets/COR49_Billancourt/COR49_Billancourt.xml';
+import config from './assets/projects/project-1/overall/published/360_assets/COR49_Billancourt/COR49_Billancourt.xml';
+import license from './assets/projects/project-1/overall/published/license.lic';
+import './assets/projects/project-1/overall/published/imagerotator/html/css/round.css';
 
 const App = () => {
   useEffect(() => {
     window.$('#wr360PlayerId').rotator({
-      licenseFileURL: 'license.lic',
+      licenseFileURL: license,
       configFileURL: config,
-      rootPath: 'http://localhost:3000/src/',
-      graphicsPath: 'imagerotator/html/img/round',
+      graphicsPath:
+        '/assets/projects/project-1/overall/published/imagerotator/html/img/round',
       alt: '360 degree view ALT description',
       googleEventTracking: false,
     });
   }, []);
   return (
-    <div className="app">
-      <section
-        id="wr360PlayerId"
-        className="wr360_player"
-        style={{ backgroundColor: '#FFFFFF' }}
-      ></section>
-    </div>
+    <section
+      id="wr360PlayerId"
+      className="wr360_player"
+      style={{ backgroundColor: '#FFFFFF' }}
+    ></section>
   );
 };
 
