64fd3b6bf6299ab29584ee5884d5e25105c20259
NAM TRUONG DUONG <namtruong@namtruong-pc>
2020-12-12 17:47:42 +0700

    add switch button && rewrited css by styled-component && add project-2 && add feature reload xml file and flow index image after reload

--- src/App.js
+++ src/App.js
@@ -2,33 +2,37 @@ import { useEffect } from 'react';
 
 import './assets/js/imagerotator.js';
 import { initializeAPIDemo } from './assets/projects/project-1/overall/action';
-import config from './assets/projects/project-1/overall/published/360_assets/COR49_Billancourt/COR49_Billancourt.xml';
 import license from './assets/projects/project-1/overall/published/license.lic';
 import './assets/projects/project-1/overall/published/imagerotator/html/css/round.css';
+
 import Menu from './component/menu';
+import SwitchBtn from './component/SwitchBtn';
 
 const App = () => {
-  
   useEffect(() => {
     window.$('#wr360PlayerId').rotator({
       licenseFileURL: license,
-      configFileURL: config,
+      configFileURL:
+        '/assets/projects/project-1/overall/published/360_assets/COR49_Billancourt/COR49_Billancourt.xml',
       graphicsPath:
         '/assets/projects/project-1/overall/published/imagerotator/html/img/round',
       alt: '360 degree view ALT description',
       googleEventTracking: false,
-      apiReadyCallback: function (api, isFullScreen) {
-        if (!isFullScreen) initializeAPIDemo(api);
+      apiReadyCallback: (api, isFullScreen) => {
+        if (!isFullScreen) {
+          initializeAPIDemo(api);
+        }
       },
     });
   }, []);
 
   const onHandleClick = (name) => {
     console.log(name);
-  }
+  };
 
   return (
     <>
+      <SwitchBtn />
       <Menu onHandleClick={onHandleClick} />
       <section
         id="wr360PlayerId"
--- src/assets/projects/project-1/overall/action.js
+++ src/assets/projects/project-1/overall/action.js
@@ -10,4 +10,24 @@ export const initializeAPIDemo = (api) => {
         break;
     }
   });
+
+  let config = {
+    status: false,
+    path:
+      '/assets/projects/project-1/overall/published/360_assets/COR49_Billancourt/COR49_Billancourt.xml',
+  };
+
+  window.$('#switchBtn').click((e) => {
+    e.preventDefault();
+    if (config.status) {
+      config.status = false;
+      config.path =
+        '/assets/projects/project-1/overall/published/360_assets/COR49_Billancourt/COR49_Billancourt.xml';
+    } else {
+      config.status = true;
+      config.path =
+        '/assets/projects/project-2/overall/IntegrationTemplates/360_assets/VansShoe/VansShoe.xml';
+    }
+    api.reload(config.path, null, null, api.images.getCurrentImageIndex());
+  });
 };
