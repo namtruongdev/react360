1ba8921ebecc693f7199a29b9f115dca60f3f325
DUONG NAM TRUONG <namtruongdev@gmail.com>
2020-12-16 18:47:29 +0700

    update hoverImage

--- src/assets/projects/project-1/overall/action.js
+++ src/assets/projects/project-1/overall/action.js
@@ -5,6 +5,9 @@ export const initializeAPIDemo = (api) => {
         window.location.assign(hotspotInfo.url);
         break;
 
+      case 5:
+        break;
+
       default:
         alert('haha');
         break;
@@ -17,8 +20,24 @@ export const initializeAPIDemo = (api) => {
       '/assets/projects/project-1/overall/published/360_assets/COR49_Billancourt/COR49_Billancourt.xml',
   };
 
+  window.$(document).ready(function () {
+    window.$('.spot3_indicator').css('opacity', '0');
+  });
+
+  window.$('.spot3_indicator').hover(
+    function () {
+      window.$(this).css('opacity', '1');
+      window.$(this).css('transition', 'all .5s ease');
+    },
+    function () {
+      window.$(this).css('opacity', '0');
+      window.$(this).css('transition', 'all .5s ease');
+    }
+  );
+
   window.$('#switchBtn').click((e) => {
     e.preventDefault();
+
     if (config.status) {
       config.status = false;
       config.path =
@@ -29,7 +48,27 @@ export const initializeAPIDemo = (api) => {
         '/assets/projects/project-2/overall/IntegrationTemplates/360_assets/VansShoe/VansShoe.xml';
     }
 
-    api.reload(config.path, null, null, api.images.getCurrentImageIndex());
+    api.reload(
+      config.path,
+      null,
+      () => {
+        window.$(document).ready(function () {
+          window.$('.spot3_indicator').css('opacity', '0');
+        });
+
+        window.$('.spot3_indicator').hover(
+          function () {
+            window.$(this).css('opacity', '1');
+            window.$(this).css('transition', 'all .5s ease');
+          },
+          function () {
+            window.$(this).css('transition', 'all .5s ease');
+
+            window.$(this).css('opacity', '0');
+          }
+        );
+      },
+      api.images.getCurrentImageIndex()
+    );
   });
-  return false;
 };
