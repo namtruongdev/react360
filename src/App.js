c7cf99ec90c8d214cdd4e53583b421164b337c03
DUONG NAM TRUONG <namtruongdev@gmail.com>
2020-12-16 10:32:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,32 +9,32 @@ import Menu from './component/menu';
 import SwitchBtn from './component/SwitchBtn';
 
 const App = () => {
-  useEffect(() => {
-    window.$('#wr360PlayerId').rotator({
-      licenseFileURL: license,
-      configFileURL:
-        '/assets/projects/project-1/overall/published/360_assets/COR49_Billancourt/COR49_Billancourt.xml',
-      graphicsPath:
-        '/assets/projects/project-1/overall/published/imagerotator/html/img/round',
-      alt: '360 degree view ALT description',
-      googleEventTracking: false,
-      apiReadyCallback: (api, isFullScreen) => {
-        if (!isFullScreen) {
-          initializeAPIDemo(api);
-          for (let i = 1; i <= 36; i++) {
-            let fileName = `Filename_0${i}.jpg`;
-            if (i >= 10) fileName = `Filename_${i}.jpg`;
-            window
-              .$('<img>')
-              .attr({
-                src: `/assets/projects/project-2/overall/IntegrationTemplates/360_assets/VansShoe/images/${fileName}`,
-              })
-              .appendTo('.hidden');
-          }
-        }
-      },
-    });
-  }, []);
+  const viewer = window.WR360.ImageRotator.Create('wr360PlayerId');
+  viewer.licenseFileURL = license;
+  viewer.settings.configFileURL =
+    '/assets/projects/project-1/overall/published/360_assets/COR49_Billancourt/COR49_Billancourt.xml';
+  viewer.settings.graphicsPath =
+    '/assets/projects/project-1/overall/published/imagerotator/html/img/round';
+  viewer.settings.alt = '360 degree view ALT description';
+  viewer.settings.googleEventTracking = false;
+  viewer.settings.apiReadyCallback = (api, isFullScreen) => {
+    if (!isFullScreen) {
+      initializeAPIDemo(api);
+    }
+    for (let i = 1; i <= 36; i++) {
+      let fileName = `Filename_0${i}.jpg`;
+      if (i >= 10) fileName = `Filename_${i}.jpg`;
+      window
+        .$('<img>')
+        .attr({
+          src: `/assets/projects/project-2/overall/IntegrationTemplates/360_assets/VansShoe/images/${fileName}`,
+        })
+        .appendTo('.hidden');
+    }
+    return false;
+  };
+
+  viewer.runImageRotator();
 
   const onHandleClick = (name) => {
     console.log(name);
