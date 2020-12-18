a691a2f31a733f22d7bf3e8036a7c90aa6fa491a
DUONG NAM TRUONG <namtruongdev@gmail.com>
2020-12-18 14:36:05 +0700

    big update: add map highlight

--- src/App.js
+++ src/App.js
@@ -50,7 +50,135 @@ const App = () => {
       ></section>
 
       <img style={{ opacity: 0 }} id="popup" src="#" alt="popup" />
-
+      <div className="floor-4" style={{ opacity: 0 }}>
+        <span
+          className="closeFloor4"
+          style={{
+            fontSize: 30,
+            display: 'block',
+            width: 40,
+            height: 40,
+            textAlign: 'center',
+            lineHeight: 1,
+            position: 'absolute',
+            borderRadius: '50%',
+            top: 30,
+            right: 30,
+            cursor: 'pointer',
+            boxShadow: '0px 0px 16px 0px rgba(0,0,0,0.75)',
+          }}
+        >
+          &#9932;
+        </span>
+        <img
+          className="test"
+          id="floor4-image"
+          src="assets/projects/project-1/overall/published/360_assets/COR49_Billancourt/images/floorplan.jpg"
+          useMap="#image-map"
+          alt=""
+        />
+        <map name="image-map">
+          <area
+            target=""
+            alt="utility"
+            title="utility"
+            href="#"
+            coords="311,33,406,33,408,192,313,94"
+            shape="poly"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="kitchen"
+            title="kitchen"
+            href="#"
+            coords="197,155,310,32"
+            shape="rect"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="family room"
+            title="family room"
+            href="#"
+            coords="200,156,199,290,375,290,439,227,312,99,311,155"
+            shape="poly"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="conservatory"
+            title="conservatory"
+            href="#"
+            coords="82,169,200,170,199,282,181,284,182,314,81,315"
+            shape="poly"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="living room"
+            title="living room"
+            href="#"
+            coords="198,297,375,509"
+            shape="rect"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="bedroom 2"
+            title="bedroom 2"
+            href="#"
+            coords="481,299,645,451"
+            shape="rect"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="hall"
+            title="hall"
+            href="#"
+            coords="536,209,464,208,380,292,380,509,456,509,457,452,474,453,475,292,536,292"
+            shape="poly"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="bathroom"
+            title="bathroom"
+            href="#"
+            coords="540,208,644,292"
+            shape="rect"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="bedroom 1"
+            title="bedroom 1"
+            href="#"
+            coords="486,31,645,201"
+            shape="rect"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="en-suite"
+            title="en-suite"
+            href="#"
+            coords="409,113,482,33"
+            shape="rect"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+          <area
+            target=""
+            alt="wardrobe"
+            title="wardrobe"
+            href="#"
+            coords="411,118,483,117,483,202,464,204,445,222,410,192"
+            shape="poly"
+            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"ff0000","fillOpacity":0.6}'
+          />
+        </map>
+      </div>
       <div className="hidden" style={{ display: 'block' }}></div>
     </>
   );
--- src/assets/projects/project-1/overall/action.js
+++ src/assets/projects/project-1/overall/action.js
@@ -1,14 +1,45 @@
 export const initializeAPIDemo = (api) => {
   api.hotspots.onAction(({ hotspotInfo }) => {
+    const switchBtn = document.querySelector('#switchBtn');
+    const menu = document.querySelector('#menu');
     switch (hotspotInfo.clickDataParam) {
-      case 3:
+      case 4:
+        const floor4 = document.querySelector('.floor-4');
+        const closeFloor4 = document.querySelector('.closeFloor4');
+        switchBtn.style.display = 'none';
+        menu.style.display = 'none';
+        floor4.style.background = '#fff';
+        floor4.style.display = 'flex';
+        floor4.style.justifyContent = 'center';
+        floor4.style.alignItems = 'center';
+        floor4.style.width = '100vw';
+        floor4.style.height = '100vh';
+        floor4.style.transition = 'opacity 1s ease';
+        floor4.style.position = 'absolute';
+        floor4.style.left = '0';
+        floor4.style.top = '50%';
+        floor4.style.transform = 'translateY(-50%)';
+        floor4.style.zIndex = '11';
+        floor4.style.opacity = 1;
+        window.$(function () {
+          window.$('.test').maphilight();
+        });
+        closeFloor4.addEventListener('click', () => {
+          floor4.style.opacity = '0';
+          switchBtn.style.display = 'block';
+          menu.style.display = 'block';
+          setTimeout(() => {
+            floor4.style.zIndex = -1;
+          }, 1000);
+        });
+        break;
+      case 6:
         window.location.assign(hotspotInfo.url);
         break;
 
-      case 5:
+      case 7:
         const img = document.querySelector('#popup');
-        const switchBtn = document.querySelector('#switchBtn');
-        const menu = document.querySelector('#menu');
+
         switchBtn.style.display = 'none';
         menu.style.display = 'none';
         img.src =
@@ -21,6 +52,7 @@ export const initializeAPIDemo = (api) => {
         img.style.transform = 'translateY(-50%)';
         img.style.zIndex = '11';
         img.style.opacity = 1;
+        img.style.cursor = 'pointer';
         img.addEventListener('click', () => {
           img.style.opacity = '0';
           switchBtn.style.display = 'block';
